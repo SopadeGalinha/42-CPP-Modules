84484866e335c672ec6857f151378c70f6deb7ba
root <root@Marla.>
2023-12-01 01:38:04 +0000

    Megafone OK

--- Module-00/ex00/megaphone.cpp
+++ Module-00/ex00/megaphone.cpp
@@ -10,10 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <iostream>
-# include <string>
+#include <iostream>
 
-int main() {
-    std::cout << "Olá, mundo!" << std::endl;
-    return 0;
+/* 
+$>./megaphone "shhhhh... I think the students are asleep..."
+SHHHHH... I THINK THE STUDENTS ARE ASLEEP...	->	expected output
+SHHHHH... I THINK THE STUDENTS ARE ASLEEP...	->	my output
+
+$>./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
+DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.	->	expected output
+DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.	->	my output
+
+$>./megaphone
+* LOUD AND UNBEARABLE FEEDBACK NOISE *	->	expected output
+* LOUD AND UNBEARABLE FEEDBACK NOISE *	->	my output
+$> */
+
+int main(int ac, char **av) {
+	if (ac == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else {
+		for (int i = 1; i < ac; ++i) {
+			for (int j = 0; av[i][j]; ++j) {
+				std::cout << (char)(av[i][j] >= 'a' && av[i][j] <= 'z'\
+					? av[i][j] - 32 : av[i][j]);
+			}
+		}
+	}
 }
